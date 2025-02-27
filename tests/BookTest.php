a84ab6496bde350ede18df365c7b11838cf4be57
KhTaeron <120678815+KhTaeron@users.noreply.github.com>
2025-02-27 09:37:16 +0100

    Règles métiers Book

--- tests/BookTest.php
+++ tests/BookTest.php
@@ -24,4 +24,33 @@ class BookTest extends TestCase
         $this->assertEquals('Broché', $book->getFormat());
         $this->assertTrue($book->isAvailable());
     }
+
+    public function testBookCreation_InvalidIsbn()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $book = new Book();
+        $book->setIsbn('1234'); // ISBN invalide
+    }
+
+    public function testBookCreation_MissingFields()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $book = new Book();
+        $book->setIsbn('978-2755673135'); 
+    }
+
+    public function testBookCreation_InvalidFormat()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $book = new Book();
+        $book->setIsbn('978-2755673135');
+        $book->setTitle('Fourth Wing');
+        $book->setAuthor('Rebecca Yarros');
+        $book->setPublisher('Hugo Roman');
+        $book->setFormat('Inconnu'); // Format invalide
+        $book->setAvailable(true);
+    }
 }
