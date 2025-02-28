aa20b8796e7833edd33c09830998bb038afb665c
KhTaeron <120678815+KhTaeron@users.noreply.github.com>
2025-02-28 15:46:42 +0100

    Create Reservation test

---
+++ tests/ReservationsTests/ReservationTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\SubscribersTests;
+
+use DateTime;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use App\Entity\Subscriber;
+
+class ReservationTest extends TestCase
+{
+    public function testReservationCreation(): void
+    {
+        $subscriber = new Subscriber();
+        $subscriber->setCode('9785');
+        $subscriber->setLastname('Dupont');
+        $subscriber->setFirstname('Jean');
+        $subscriber->setBirthdate('01-01-1990');
+        $subscriber->setCivilite('M');
+
+        $reservationDate = new DateTime();
+        $reservation = new Reservation($subscriber, $reservationDate);
+
+        $this->assertInstanceOf(Reservation::class, $reservation);
+        $this->assertEquals(4, $reservation->getExpirationDate()->diff($reservationDate)->m);
+    }
+}
